2a5b2a873f00237d03b7c4758dfdafad6360fc8a
Tim Tomes <tjt1980@gmail.com>
2024-03-01 13:52:21 -0500

    Updated the python import script.

--- scripts/send-results-to-defectdojo.py
+++ scripts/send-results-to-defectdojo.py
@@ -7,23 +7,23 @@ defect_dojo_domain = os.getenv('DEFECT_DOJO_DOMAIN')
 defect_dojo_engagement_value = os.getenv('DEFECT_DOJO_ENGAGEMENT_VALUE')
 defect_dojo_scan_type = os.getenv('DEFECT_DOJO_SCAN_TYPE')
 
-url = "https://{}/api/v2/import-scan/".format(defect_dojo_domain)
-headers = {"Authorization": "Token {}".format(defect_dojo_token)}
+url = f"https://{defect_dojo_domain}/api/v2/import-scan/"
+headers = {"Authorization": f"Token {defect_dojo_token}"}
 
 files = {'file': open(sys.argv[1], 'rb').read()}
 
 values = {
-    'engagement' : defect_dojo_engagement_value,
+    'engagement': defect_dojo_engagement_value,
     'scan_type': defect_dojo_scan_type
-    }
+}
 
 try:
     r = requests.post(url, headers=headers, files=files, data=values, verify=True)
     r.raise_for_status()
     if (r.status_code == 201):
-        print("Scan Import was successful\n")
+        print("Scan import was successful.\n")
         print(r.json())
     else:
-        print("Something went wrong..")
+        print("Something went wrong.")
 except requests.exceptions.RequestException as e:  # This is the correct syntax
     raise SystemExit(e)
